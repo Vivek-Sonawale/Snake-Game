3266a0faf44c243648f14679262b5d3578eaf5ac
Vivek-Sonawale <sonawalevivek777@gmail.com>
2023-11-24 21:36:41 +0530

    Fix Error Related to ImageIcon

--- Snake_Game/src/SnakeGame/Board.java
+++ Snake_Game/src/SnakeGame/Board.java
@@ -1,4 +1,5 @@
 package SnakeGame;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,32 +8,34 @@ public class Board extends JPanel {
 	Image apple;
 	Image dot;
 	Image head;
+
 	Board() {
 		this.setBackground(Color.BLACK); // setBackGround to Black
 		this.setFocusable(true);
 		loadimages();
 		initgame();
-		
+
 	}
-	
+
 	public void loadimages() {
-		ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("D:\\JAVA\\YearWeekEnd\\Snake-Game\\Snake_Game\\src\\icons\\apple.png"));
+		ImageIcon i1 = new ImageIcon(
+				ClassLoader.getSystemResource("SnakeGame/icons/apple.png"));
 		apple = i1.getImage();
-		
-		ImageIcon i2= new ImageIcon(ClassLoader.getSystemResource("D:\\JAVA\\YearWeekEnd\\Snake-Game\\Snake_Game\\src\\icons\\dot.png"));
+
+		ImageIcon i2 = new ImageIcon(
+				ClassLoader.getSystemResource("SnakeGame/icons/dot.png"));
 		dot = i2.getImage();
-		
-		ImageIcon i3= new ImageIcon(ClassLoader.getSystemResource("D:\\JAVA\\YearWeekEnd\\Snake-Game\\Snake_Game\\src\\icons\\head.png"));
+
+		ImageIcon i3 = new ImageIcon(
+				ClassLoader.getSystemResource("SnakeGame/icons/head.png"));
 		head = i3.getImage();
-		
+
 	}
 
 	public void initgame() {
 		dots = 3;
 		for (int i = 0; i < dots; i++) {
-			
+
 		}
 	}
-
-	
 }
